764c3773eff1faab0c21ebbea13eb9e5b2ab797c
The Hermit™ <71921416+PageSecOnd@users.noreply.github.com>
2026-08-10 17:23:47 +0800

    Default to vanilla-safe player limit

--- SlayTheSpire2.LAN.Multiplayer.Reforged/Models/SettingsModel.cs
+++ SlayTheSpire2.LAN.Multiplayer.Reforged/Models/SettingsModel.cs
@@ -1,6 +1,7 @@
 using System.Text.Json.Serialization;
 using MegaCrit.Sts2.Core.Saves;
 using Steamworks;
+using SlayTheSpire2.LAN.Multiplayer.Reforged.Helpers;
 
 namespace SlayTheSpire2.LAN.Multiplayer.Reforged.Models
 {
@@ -10,7 +11,7 @@ namespace SlayTheSpire2.LAN.Multiplayer.Reforged.Models
     public class SettingsModel : ISaveSchema
     {
         [JsonPropertyName("host_port")] public ushort HostPort { get; set; } = 33771;
-        [JsonPropertyName("host_max_players")] public int HostMaxPlayers { get; set; } = 4;
+        [JsonPropertyName("host_max_players")] public int HostMaxPlayers { get; set; } = LanProtocolPolicy.MaxPlayers;
         [JsonPropertyName("ip_address")] public string IPAddress { get; set; } = "127.0.0.1";
         [JsonPropertyName("remember_join_address")] public bool RememberJoinAddress { get; set; } = true;
         [JsonPropertyName("connect_timeout_seconds")] public int ConnectTimeoutSeconds { get; set; } = 10;
@@ -20,5 +21,4 @@ namespace SlayTheSpire2.LAN.Multiplayer.Reforged.Models
         public int SchemaVersion { get; set; }
     }
 }
-
 
