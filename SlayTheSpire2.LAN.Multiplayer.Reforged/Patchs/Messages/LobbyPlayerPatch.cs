e13d3fd92d1eef3441a8b266eef39a4ad1f4561e
The Hermit™ <71921416+PageSecOnd@users.noreply.github.com>
2026-08-10 10:46:55 +0800

    Adapt lobby player serialization for STS2 0.110.1

--- SlayTheSpire2.LAN.Multiplayer.Reforged/Patchs/Messages/LobbyPlayerPatch.cs
+++ SlayTheSpire2.LAN.Multiplayer.Reforged/Patchs/Messages/LobbyPlayerPatch.cs
@@ -1,6 +1,7 @@
-﻿using HarmonyLib;
+using HarmonyLib;
 using MegaCrit.Sts2.Core.Entities.Multiplayer;
 using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Multiplayer;
 using MegaCrit.Sts2.Core.Multiplayer.Serialization;
 using MegaCrit.Sts2.Core.Unlocks;
 using SlayTheSpire2.LAN.Multiplayer.Reforged.Helpers;
@@ -10,32 +11,34 @@ using SlayTheSpire2.LAN.Multiplayer.Reforged.Helpers;
 
 namespace SlayTheSpire2.LAN.Multiplayer.Reforged.Patchs.Messages
 {
-    [HarmonyPatch(typeof(LobbyPlayer), "Serialize")]
-    internal class LobbyPlayerSerializePatch
+    [HarmonyPatch(typeof(StartRunLobbyPlayer), "Serialize")]
+    internal class StartRunLobbyPlayerSerializePatch
     {
-        private static bool Prefix(LobbyPlayer __instance, PacketWriter writer)
+        private static bool Prefix(StartRunLobbyPlayer __instance, PacketWriter writer)
         {
             writer.WriteULong(__instance.id);
             PacketHelper.WriteVarInt(writer, (uint)__instance.slotId);
             writer.WriteModel(__instance.character);
             writer.Write(__instance.unlockState);
             writer.WriteInt(__instance.maxMultiplayerAscensionUnlocked);
+            writer.Write(__instance.versionInfo);
             writer.WriteBool(__instance.isReady);
 
             return false;
         }
     }
 
-    [HarmonyPatch(typeof(LobbyPlayer), "Deserialize")]
-    internal class LobbyPlayerDeserializePatch
+    [HarmonyPatch(typeof(StartRunLobbyPlayer), "Deserialize")]
+    internal class StartRunLobbyPlayerDeserializePatch
     {
-        private static bool Prefix(ref LobbyPlayer __instance, PacketReader reader)
+        private static bool Prefix(ref StartRunLobbyPlayer __instance, PacketReader reader)
         {
             __instance.id = reader.ReadULong();
             __instance.slotId = (int)PacketHelper.ReadVarInt(reader);
             __instance.character = reader.ReadModel<CharacterModel>();
             __instance.unlockState = reader.Read<SerializableUnlockState>();
             __instance.maxMultiplayerAscensionUnlocked = reader.ReadInt();
+            __instance.versionInfo = reader.Read<PeerVersionInfo>();
             __instance.isReady = reader.ReadBool();
 
             return false;
